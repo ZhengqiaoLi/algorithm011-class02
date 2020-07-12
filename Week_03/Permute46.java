8bc666f19bbd01df0c8b9f13c37d849e59be8f9d
lizhengqiao <729089549@qq.com>
2020-07-12 23:33:36 +0800

    optimize code

--- Week_03/Permute46.java
+++ Week_03/Permute46.java
@@ -6,25 +6,29 @@ import java.util.List;
 import java.util.Stack;
 
 public class Permute46 {
+    // 回溯方法
     private List<List<Integer>> out = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
-        generate(new Stack<>(), nums, nums.length);
+        boolean[] used = new boolean[nums.length];
+        generate(new Stack<>(), nums, nums.length, used);
         return out;
     }
 
-    private void generate(Stack<Integer> result, int[] nums, int length) {
-        //退出条件
+    private void generate(Stack<Integer> result, int[] nums, int length, boolean[] used) {
         if(result.size() == length) {
             out.add(new ArrayList<>(result));
             return;
         }
         for(int i = 0; i < length; i++) {
             //跳过已经处理过的数据
-            if(!result.contains(nums[i])) {
+            if(!used[i]) {
                 //将当前值压栈
                 result.push(nums[i]);
+                used[i] = true;
                 //继续下一个数据
-                generate(result, nums, length);
+                generate(result, nums, length, used);
+                //撤销
+                used[i] = false;
                 //将满足上述退出条件的数据的最后一个数据弹出，重新压入最新的下一个数据
                 result.pop();
             }
